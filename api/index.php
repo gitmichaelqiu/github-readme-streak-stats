04e02727b4fda90cd0562a5a4b746bc3e7cbb5c1
Michael Y. Qiu <altmickymike@outlook.com>
2026-05-14 12:30:47 +0800

    fix(php): declare first then disable error reporting

--- api/index.php
+++ api/index.php
@@ -1,7 +1,8 @@
 <?php
-error_reporting(E_ALL & ~E_DEPRECATED);
 
 declare(strict_types=1);
+
+error_reporting(E_ALL & ~E_DEPRECATED);
 
 // load functions
 require_once dirname(__DIR__, 1) . "/vendor/autoload.php";
